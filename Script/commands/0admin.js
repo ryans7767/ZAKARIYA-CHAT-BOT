47005e5731faed62e3420c0fb0a6b252a75d4f99
ryans7767 <itzryansgaming71@gmail.com>
2025-12-02 19:08:07 +0600

    Update 0admin.js

--- Script/commands/0admin.js
+++ Script/commands/0admin.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "0admin",
 	version: "1.0.5",
 	hasPermssion: 3,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "ZAKARIYA ☢️_𖣘 -CHAT ⚠️ BOT_ ☢️",
 	description: "Admin Config",
 	commandCategory: "Admin",
 	usages: "Admin",
